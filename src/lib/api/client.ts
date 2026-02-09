3309924f213dbea0cfa4b3ec0c5b25b54a9565e1
ahny <ahny@ahnyui-MacBookPro.local>
2026-02-09 16:28:50 +0900

    chroe: change base url

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://3.37.155.46/api/v1";
+const API_BASE_URL = "https://seolstudy.duckdns.org/api/v1";
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
